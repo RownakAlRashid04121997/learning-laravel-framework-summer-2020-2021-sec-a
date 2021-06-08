0831b959e0f75c0d834c8f21a598107bd871a346
SunanRashidAIUB <sunanrashid@gmail.com>
2021-06-08 12:31:37 +0600

    created getlist function in usercontroller

--- lab3/app/Http/Controllers/UserController.php
+++ lab3/app/Http/Controllers/UserController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function getList(){
+		 $users= [
+		 ['id'=>1, 'username'=>'abc', 'email'=>'abc@gmail.com'];
+		 ['id'=>2, 'username'=>'def', 'email'=>'def@gmail.com'];
+		 ['id'=>3, 'username'=>'ghi', 'email'=>'def@gmail.com'];
+		 ['id'=>4, 'username'=>'jkl', 'email'=>'jkl@gmail.com'];
+		 ]
+	}
 }
